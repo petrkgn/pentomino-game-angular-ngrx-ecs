67c198ffecc1c51bdf9551e11ff06ddd8040a604
petrkgn <petrsdk@gmail.com>
2024-03-19 20:25:09 +0500

    chore(board): board component refactoring

--- src/game/layers/board/board.component.ts
+++ src/game/layers/board/board.component.ts
@@ -13,21 +13,27 @@ import { PentominoActions } from '../../store/game/game.actions';
 import { GameObjectsIds } from '../../constants/game-objects-ids.enum';
 import { ComponentType } from '../../constants/component-type.enum';
 import { ResizeService } from '../../services/resize.service';
+import { CanvasParams } from '../../interfaces/canvas-params';
+import { CanvasParamsDirective } from '../../directives/canvas-params.directive';
 
 
 @Component({
   selector: 'game-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css'],
-  imports: [BonudsElementDirective],
+  imports: [
+    BonudsElementDirective,
+    CanvasParamsDirective
+   ],
   standalone: true,
 })
 export class BoardComponent implements AfterViewInit {
   private readonly store = inject(Store);
   private readonly resizeService = inject(ResizeService);
-  private readonly canvas = viewChild.required<ElementRef<HTMLCanvasElement>>('canvas')
-  boardLayer!: HTMLCanvasElement;
-  ctx!: CanvasRenderingContext2D | null;
+
+
+  private canvasParams!: CanvasParams;
+
   cellSize = 32;
   numRows = BoardsSize.firstLevel.length;
   numCols = BoardsSize.firstLevel[0].length;
@@ -37,10 +43,10 @@ export class BoardComponent implements AfterViewInit {
   constructor() {}
 
   ngAfterViewInit() {
-    this.boardLayer = this.canvas().nativeElement;
-    this.ctx = this.boardLayer.getContext('2d');
-    this.boardLayer.width = this.numCols * this.cellSize;
-    this.boardLayer.height = this.numRows * this.cellSize;
+  
+  
+    this.canvasParams.layer.width = this.numCols * this.cellSize;
+    this.canvasParams.layer.height = this.numRows * this.cellSize;
     // this.drawGrid();
 
     this.resizeService
@@ -48,10 +54,9 @@ export class BoardComponent implements AfterViewInit {
       .pipe(
         tap((value) => {
           const ratio = Math.ceil(value);
-          // console.log('ratio', ratio);
           this.cellSize = 32 * ratio;
-          this.boardLayer.width = this.numCols * this.cellSize;
-          this.boardLayer.height = this.numRows * this.cellSize;
+          this.canvasParams.layer.width = this.numCols * this.cellSize;
+          this.canvasParams.layer.height = this.numRows * this.cellSize;
           this.getBoardPosition();
           this.drawGrid(ratio);
         })
@@ -59,9 +64,13 @@ export class BoardComponent implements AfterViewInit {
       .subscribe();
   }
 
+  onCanvasParams(params: CanvasParams): void {
+    this.canvasParams = params;
+  }
+
   private getBoardPosition(): void {
-    this.boardTop = this.boardLayer.getBoundingClientRect().top;
-    this.boardLeft = this.boardLayer.getBoundingClientRect().left;
+    this.boardTop = this.canvasParams.layer.getBoundingClientRect().top;
+    this.boardLeft = this.canvasParams.layer.getBoundingClientRect().left;
     this.store.dispatch(
       PentominoActions.updateComponentData({
         entityId: GameObjectsIds.BOARD,
@@ -72,25 +81,25 @@ export class BoardComponent implements AfterViewInit {
   }
 
   private drawGrid(ratio: number): void {
-    if (!this.ctx) return;
+    if (!this.canvasParams.ctx) return;
 
-    this.ctx.clearRect(0, 0, this.boardLayer.width, this.boardLayer.height);
-    this.ctx.lineWidth = 0.6 * ratio;
+    this.canvasParams.ctx.clearRect(0, 0, this.canvasParams.layer.width, this.canvasParams.layer.height);
+    this.canvasParams.ctx.lineWidth = 0.6 * ratio;
     for (let i = 0; i <= this.numRows; i++) {
       const y = i * this.cellSize;
-      this.ctx.beginPath();
-      this.ctx.moveTo(0, y);
-      this.ctx.lineTo(this.boardLayer.width, y);
-      this.ctx.stroke();
+      this.canvasParams.ctx.beginPath();
+      this.canvasParams.ctx.moveTo(0, y);
+      this.canvasParams.ctx.lineTo(this.canvasParams.width, y);
+      this.canvasParams.ctx.stroke();
     }
 
     // Вертикальные линии
     for (let i = 0; i <= this.numCols; i++) {
       const x = i * this.cellSize;
-      this.ctx.beginPath();
-      this.ctx.moveTo(x, 0);
-      this.ctx.lineTo(x, this.boardLayer.height);
-      this.ctx.stroke();
+      this.canvasParams.ctx.beginPath();
+      this.canvasParams.ctx.moveTo(x, 0);
+      this.canvasParams.ctx.lineTo(x, this.canvasParams.height);
+      this.canvasParams.ctx.stroke();
     }
   }
 }
