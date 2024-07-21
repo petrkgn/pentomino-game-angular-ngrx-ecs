046ae7e9fff9992e583cd7da37f7f9ef8fff8bef
Petr Balashenko <lastmail@yandex.ru>
2024-07-21 12:34:18 +0500

    chore(canvas directive): refactoring

--- src/game/directives/canvas-params.directive.ts
+++ src/game/directives/canvas-params.directive.ts
@@ -2,63 +2,43 @@ import {
   AfterViewInit,
   Directive,
   ElementRef,
-  Input,
-  Output,
   inject,
   output,
+  input,
 } from "@angular/core";
-import { WINDOW } from "@ng-web-apis/common";
+import { Observable, tap } from "rxjs";
 
 import { CanvasParams } from "../types/canvas-params";
 import { ResizeService } from "../services/resize.service";
-import { tap } from "rxjs";
 
 @Directive({
   selector: "[canvasParams]",
   standalone: true,
 })
 export class CanvasParamsDirective implements AfterViewInit {
-  @Input() canvasCss: string = "";
-  private readonly window = inject(WINDOW);
   private readonly elRef = inject<ElementRef<HTMLCanvasElement>>(ElementRef);
   private readonly resizeService = inject(ResizeService);
 
+  canvasCss = input<string>("");
   canvasParams = output<CanvasParams>();
 
   ngAfterViewInit() {
-    this.initCanvasParams();
-
-    this.resizeService
-      .calculateScaleRatio(32, 20)
-      .pipe(
-        tap(() => {
-          this.initCanvasParams();
-        })
-      )
-      .subscribe();
+    this.initializeCanvasParams();
+    this.handleResize().subscribe();
   }
 
-  private initCanvasParams(): void {
+  private handleResize(): Observable<number> {
+    return this.resizeService
+      .calculateScaleRatio(32, 20)
+      .pipe(tap(() => this.initializeCanvasParams()));
+  }
+
+  private initializeCanvasParams(): void {
     const canvas = this.elRef.nativeElement;
-
-    canvas.style.cssText = `${this.canvasCss}; width: 100vw; height: 100vh;`;
-
-    const ctx = canvas.getContext("2d")!;
-    const rect = canvas.getBoundingClientRect();
-
-    const width = (canvas.width =
-      Math.round(devicePixelRatio * rect.right) -
-      Math.round(devicePixelRatio * rect.left));
-    const height = (canvas.height =
-      Math.round(devicePixelRatio * rect.bottom) -
-      Math.round(devicePixelRatio * rect.top));
-
-    const canvasCenter = {
-      x: this.window.innerWidth * 0.5,
-      y: this.window.innerHeight * 0.5,
-    };
-
-    ctx.imageSmoothingEnabled = true;
+    this.applyCanvasStyles(canvas);
+    const ctx = this.getCanvasContext(canvas);
+    const { width, height } = this.setCanvasDimensions(canvas);
+    const canvasCenter = this.calculateCanvasCenter(width, height);
 
     const canvasParams: CanvasParams = {
       ctx,
@@ -67,6 +47,46 @@ export class CanvasParamsDirective implements AfterViewInit {
       height,
       canvas,
     };
+
     this.canvasParams.emit(canvasParams);
+  }
+
+  private applyCanvasStyles(canvas: HTMLCanvasElement): void {
+    canvas.style.cssText = `${this.canvasCss()}; width: 100vw; height: 100vh;`;
+  }
+
+  private getCanvasContext(
+    canvas: HTMLCanvasElement
+  ): CanvasRenderingContext2D {
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      throw new Error("2D context not supported or canvas already initialized");
+    }
+    ctx.imageSmoothingEnabled = true;
+    return ctx;
+  }
+
+  private setCanvasDimensions(canvas: HTMLCanvasElement): {
+    width: number;
+    height: number;
+  } {
+    const rect = canvas.getBoundingClientRect();
+    const width = (canvas.width =
+      Math.round(devicePixelRatio * rect.right) -
+      Math.round(devicePixelRatio * rect.left));
+    const height = (canvas.height =
+      Math.round(devicePixelRatio * rect.bottom) -
+      Math.round(devicePixelRatio * rect.top));
+    return { width, height };
+  }
+
+  private calculateCanvasCenter(
+    width: number,
+    height: number
+  ): { x: number; y: number } {
+    return {
+      x: width * 0.5,
+      y: height * 0.5,
+    };
   }
 }
